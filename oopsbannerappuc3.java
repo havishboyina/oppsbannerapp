bb1f3305e40c3b35cf726bb982190a6e5776c57c
varshanth <varshanthreddypokala8@gmail.com>
2026-02-23 14:55:38 +0530

    i edited

--- oopsbannerappuc3.java
+++ oopsbannerappuc3.java
@@ -2,7 +2,7 @@
 public class oopsbannerappuc3 {
   
    
-    public static void main(String[] args) {
+    public static void  main(String[] args) {
       
         String[] o = {" **** ", "*    *", "*    *", " **** ", "*    *", "*    *", " **** "};
         
