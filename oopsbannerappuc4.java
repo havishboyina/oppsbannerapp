7e2bf9e327ccef11403b90244de9ee7932134aad
havish boyina <havishboyina1@gmail.com>
2026-02-23 14:37:35 +0530

    i did uc 4

---
+++ oopsbannerappuc4.java
@@ -0,0 +1,25 @@
+
+public class oopsbannerappuc4 {
+   
+    public static void main(String[] args) {
+       
+        String[] o = {" **** ", "*    *", "*    *", " **** ", "*    *", "*    *", " **** "};
+        String[] p = {"*    ", "*    ", "*    ", " ****", "*    ", "*    ", "*    "};
+        String[] s = {" **** ", "    *", " **** ", "*    ", "    *", "*    ", " **** "};
+        
+        
+        String[] banner = {
+            String.join("", o[0], o[1], p[0], s[0]),
+            String.join("", o[1], o[1], p[1], s[1]),
+            String.join("", o[2], o[2], p[2], s[2]),
+            String.join("", o[3], o[3], p[3], s[3]),
+            String.join("", o[4], o[4], p[4], s[4]),
+            String.join("", o[5], o[5], p[5], s[5]),
+            String.join("", o[6], o[6], p[6], s[6])
+        };
+        
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+}
