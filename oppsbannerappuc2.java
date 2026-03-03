d978cfd560328a12cd88b54abe52cb5754b072f6
havish boyina <havishboyina1@gmail.com>
2026-03-03 13:21:51 +0530

    Update oppsbannerappuc2.java

--- oppsbannerappuc2.java
+++ oppsbannerappuc2.java
@@ -1,6 +1,6 @@
 
 public class oppsbannerappuc2 {
-
+   
     public static void  main(String[] args) {
         System.out.println(" ****  *    ****  **** ");
         System.out.println("*    * *   *    * *   *");
